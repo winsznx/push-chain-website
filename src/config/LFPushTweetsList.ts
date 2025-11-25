c70e32038d5861a21d9d22e4b19f7e6d2553d0b8
Riyanshu Patro <riyanshu@push.org>
2025-11-25 18:51:22 +0530

    chore: add a new twitter post

--- src/config/LFPushTweetsList.ts
+++ src/config/LFPushTweetsList.ts
@@ -37,6 +37,7 @@ export const LFPushTweetsList: string[] = [
   'https://x.com/Psu353/status/1988309812598513810?s=20',
   'https://x.com/0xsakshamtyagi/status/1988349341480951828?s=20',
   'https://x.com/PranshuWeb3/status/1988477194755338364?s=20',
+  'https://x.com/hyderabaddao/status/1988272895987253657?s=20',
 
   // Till 24th Nov
   'https://x.com/techtusharojha/status/1988959987344031886?s=20',
